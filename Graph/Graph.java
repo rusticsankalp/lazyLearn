d3b6fccfc9814128deb01312c16c17b31dc58753
rusticsankalp <rusticsankalp@gmail.com>
2018-01-06 09:55:49 -0800

    First graph checkin

---
+++ Graph/Graph.java
@@ -0,0 +1,10 @@
+public class Graph 
+{
+ public Graph(int V){}
+ public Graph (In in){}
+ public int V(){return 0;}
+ public int E(){return 0;}
+ public void addEdge(int v, int w){}
+ //public Iterable<Integer> adj(int v){return NULL;}
+ public String toString(){return "";}
+}
