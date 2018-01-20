689e2436515bf67ddc4a79f298ab51d0f45a2b91
Sankalp Gupta <rusticsankalp@outlook.com>
2018-01-20 13:16:19 -0800

    graph APis added

---
+++ src/main/java/com/algs4/graph/Graph.java
@@ -0,0 +1,13 @@
+public class Graph 
+{
+ public Graph(int V){}
+ //public Graph (In in){}
+ public int V(){return 0;}
+ public int E(){return 0;}
+ public void addEdge(int v, int w){}
+ public Iterable<Integer> adj(int v){
+   Iterable<Integer> list = null;
+   return list;
+ }
+ public String toString(){return "";}
+}
